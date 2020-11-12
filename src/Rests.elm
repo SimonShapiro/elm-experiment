d75887e0a6419932c78398232208829e29fb9ed6
simonshapiro <simon.m.shapiro@gmail.com>
2020-11-12 20:08:22 +0000

    change svg to button and refactor

--- src/Rests.elm
+++ src/Rests.elm
@@ -2,7 +2,7 @@ port module Rests exposing (..)
 
 import Browser
 import Html exposing (Html, button, div, text, input, a, i, span, audio)
-import Html.Attributes exposing (href, src, controls, autoplay)
+import Html.Attributes exposing (href, src, controls, autoplay, style)
 import Html.Events exposing (onClick)
 import Html.Events exposing (onInput)
 
@@ -51,9 +51,22 @@ type Scene =
     | Training ButtonInfo SetsAndRests Bool
     | Resting SetsAndRests
 
+type alias ButtonConfig =
+    {
+        width: Int,
+        height: Int,
+        colour: String
+    }
+
+type alias Position =
+    {
+        x: Int,
+        y: Int
+    }
+
 -- [TimeConfig(60, "60sec"), TimeConfig(90, "90sec"), TimeConfig(120, "2mins"), TimeConfig(300, "5mins")]
 restGroup1: ChoiceOfRests
-restGroup1 = [RestPeriod 6 "60sec"
+restGroup1 = [RestPeriod 60 "60sec"
                 , RestPeriod 90 "90sec"
                 , RestPeriod 120 "2mins"
                 , RestPeriod 300 "5mins"
@@ -74,8 +87,13 @@ subscriptions model =
 type alias Model = 
     Scene
 
+type Workout =
+    Strength
+    | Endurance
+    | Hyper
+
 type Msg =
-    One ButtonInfo
+    One Workout
     | SetsChosen ButtonInfo String
     | RestsChosen ButtonInfo String String
     | RestingStarted SetsAndRests
@@ -100,10 +118,14 @@ main =
 update: Msg->Model->(Model, Cmd Msg)
 update msg model =
     case msg of
-        One id ->
-            (SelectingSets (workoutSelection 
-                            |> Dict.get id.id
-                            |> Maybe.withDefault defaultButton), Cmd.none)
+        One workout ->
+            case workout of
+                Strength ->
+                    (SelectingSets (ButtonInfo "pink" "S" [1, 3, 5, 6, 10] "Strength" restGroup1), Cmd.none)
+                Endurance ->
+                    (SelectingSets (ButtonInfo "purple" "E" [2, 3] "Endurance" restGroup1), Cmd.none)
+                Hyper ->
+                    (SelectingSets (ButtonInfo "red" "H" [3, 4] "Hyper" restGroup1), Cmd.none)
         SetsChosen button sets ->
             (SelectingRests button sets, Cmd.none)
         RestsChosen button sets rest ->
@@ -150,36 +172,11 @@ update msg model =
                 _ ->
                     (model, Cmd.none)
 
-    --        SelectingWorkout  (Dict.update id.id (Maybe.map (\b ->  { b | colour = "green"}   )) workoutSelection)
-            
-            --Maybe.withDefault "No user" defaultButton 
-            
-        -- "John Johnson"
 
-    -- <svg width="400" height="110">
--- --  <rect width="300" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
--- -- </svg>
--- block: Html msg
--- block = svg [width "400", height "110"][rect [width "300" height "100" style "fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" ][]]
-getButtonInfo : String -> ButtonPanel -> ButtonInfo
-getButtonInfo id buttonDict =
-        buttonDict 
-        |> Dict.get id
-        |> Maybe.withDefault defaultButton
---    |> get 
---    |> Maybe.withDefault defaultButton.colour
-
-
-header: Html msg
-header = div [class "w3-bar w3-large w3-theme-d4"]
-  [Html.a [href "#", class "w3-bar-item w3-button"][i [class "fa fa-bars"] []]
-  , span [class "w3-bar-item"] [Html.text "RESTS & SETS"]
-  ]
 view: Model -> Html Msg
 view model = 
     case model of
         SelectingWorkout buttons -> 
---        Debug.log ("There" ++ (getButtonInfo "Endurance" buttons).colour)
             div [][ div [][audio 
                 [ id "beep"
         -- src can be a local file too.
@@ -187,25 +184,15 @@ view model =
                 , controls False
                 , autoplay True
                 ] []
-
-
             ]
-            , header
-            -- below can be generated from the type
-                , div [] [shape <| getButtonInfo "Strength" buttons] -- orange
-                , div [] [shape <| getButtonInfo "Endurance" buttons]
-                , div [] [shape <| getButtonInfo "Hyper" buttons]
+            , div [] [button [class "Sbutton", onClick (One Strength)][Html.text "Strength"]]
+            , div [] [button [class "Ebutton", onClick (One Endurance)][Html.text "Endurance"]]
+            , div [] [button [class "Hbutton", onClick (One Hyper)][Html.text "Hyper"]]
             ]
         SelectingSets choice ->
-            div [][
-                div [][header]
-                , div [] (shapeChoiceOfSets choice)
-            ]
+            div [](shapeChoiceOfSets choice)
         SelectingRests choice sets -> 
-            div [][
-                div [][header]
-                    , div [] (shapeChoiceOfRests choice sets)
-                ]
+            div [](shapeChoiceOfRests choice sets)
         Training choice setsAndRest sound->
             div [][            div [][audio 
                 [ id "beep"
@@ -219,36 +206,12 @@ view model =
 
 
             ]
-             , Html.text ("Training"++" "++(String.fromInt <| setsAndRest.currentSets)++"/"++(String.fromInt <| setsAndRest.targetSets)++":"++(String.fromInt <| setsAndRest.targetRests))
-                    , div [][
-                        svg [ width "200"
-                            , height "120"
-                            , viewBox "0 0 200 120"
-                            , onClick (RestingStarted setsAndRest)
-                            ]
-                            [ rect
-                                [ x "10"
-                                , y "10"
-                                , width "180"
-                                , height "90"
-                                , rx "15"
-                                , ry "15"
-                                , fill "lightblue"
-                                , id ("Training")
-                                ]
-                                []
-                            , text_
-                                    [ x "45"
-                                    , y "63"
-                                    , fontSize "30px"
-                                ]
-                            [Html.text "Resting"
-                            ]  
-        
-                       ]
-        
-                       ]
+            , div [class "bigFont"] [Html.text ("Sets"++" "++(String.fromInt <| setsAndRest.currentSets)++"/"++(String.fromInt <| setsAndRest.targetSets))]
+            , div [][button [class "trainingButton", onClick (RestingStarted setsAndRest)]
+                            [Html.text "Start Rest"]
                     ]
+            ]  
+
         Resting setsAndRests->
             div [][div []
                 [audio 
@@ -261,133 +224,44 @@ view model =
                 , autoplay False
                 ] []
                 ]
-                , Html.text ("Resting"++(String.fromInt setsAndRests.targetRests)
-                                        ++(String.fromInt setsAndRests.currentRests))
-                ,div [][
-                        svg [ width "200"
-                            , height "120"
-                            , viewBox "0 0 200 120"
-                            , onClick (Rested setsAndRests)
-                            ]
-                            [ rect
-                                [ x "10"
-                                , y "10"
-                                , width "180"
-                                , height "90"
-                                , rx "15"
-                                , ry "15"
-                                , fill "lightblue"
-                                , id ("Rested")
-                                ]
-                                []
-                            , text_
-                                    [ x "45"
-                                    , y "63"
-                                    , fontSize "30px"
-                                ]
-                            [Html.text "Rested"
+                , div [class "bigFont"] [Html.text (convertClock2TimeString setsAndRests.targetRests setsAndRests.currentRests)]
+                ,div [][button [class "trainingButton", onClick (Rested setsAndRests)]
+                            [Html.text "Back to work"   -- "Rested"
                             ]  
-                            ]]
+                        ]
                 ]
-            
-
-
-shape: ButtonInfo->Html Msg
-shape  info =
-  svg
-    [ width "120"
-    , height "120"
-    , viewBox "0 0 120 120"
-    , onClick (One info)
-    ]
-    [ rect
-        [ x "10"
-        , y "10"
-        , width "100"
-        , height "100"
-        , rx "15"
-        , ry "15"
-        , fill info.colour
-        , id info.id
-        ]
-        []
-      , text_
-        [ x "43"
-        , y "77"
-        , fontSize "50px"
-        ]
-        [Html.text info.label]  
-    ]
 
 shapeChoiceOfSets: ButtonInfo->List (Html Msg)
-shapeChoiceOfSets button = 
+shapeChoiceOfSets buttonInfo = 
     let
-        setList = button.value
+        setList = buttonInfo.value
     in
         
         List.map (\c -> 
-        svg
-            [ width "120"
-            , height "120"
-            , viewBox "0 0 120 120"
-            , onClick (SetsChosen button (String.fromInt c))
-            ]
-            [ rect
-                [ x "10"
-                , y "10"
-                , width "100"
-                , height "100"
-                , rx "15"
-                , ry "15"
-                , fill "grey"
-                , id (String.fromInt c)
-                ]
-                []
-            , text_
-                    [ x "43"
-                    , y "77"
-                    , fontSize "50px"
-                ]
+            button [class "setButton", onClick (SetsChosen buttonInfo (String.fromInt c))] 
             [Html.text (String.fromInt c)]  
-    
-            ]
         ) setList
 
 shapeChoiceOfRests: ButtonInfo->String->List (Html Msg)
-shapeChoiceOfRests button setsChosen = 
+shapeChoiceOfRests buttonInfo setsChosen = 
     let
-        restsList = button.rest
+        restsList = buttonInfo.rest
     in
     List.map (\c -> 
-    svg
-        [ width "300"
-        , height "120"
-        , viewBox "0 0 300 120"
-        , onClick (RestsChosen button setsChosen (String.fromInt c.seconds))
-        ]
-        [ rect
-            [ x "10"
-            , y "10"
-            , width "200"
-            , height "100"
-            , rx "15"
-            , ry "15"
-            , fill "lightblue"
-            , id c.label
-            ]
-            []
-          , text_
-                [ x "43"
-                , y "77"
-                , fontSize "50px"
-            ]
-        [Html.text c.label
-        ]  
-  
-        ]
+            button [class "setButton", onClick (RestsChosen buttonInfo setsChosen (String.fromInt c.seconds))] 
+            [Html.text c.label]  
     ) restsList
--- PORTS
 
+convertClock2TimeString: Int -> Int -> String
+convertClock2TimeString rest tick =
+    let
+        secondsLeft = rest - tick
+        minutes = secondsLeft // 60
+        seconds = secondsLeft - (minutes * 60)
+    in
+        (String.padLeft 2 '0' (String.fromInt <| minutes))++":"++(String.padLeft 2 '0' (String.fromInt <| seconds))
+
+-- PORTS
 
 port playMusic : String -> Cmd msg
 
